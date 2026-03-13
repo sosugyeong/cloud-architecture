8ee369d2d2f3df0bec349fdfb9ebd6972acbf1e8
sosugyeong <sugyeong247@gmail.com>
2026-03-13 13:46:23 +0900

    refactor: 테스트 코드 삭제

--- src/test/java/com/example/member/MemberApplicationTests.java
+++ src/test/java/com/example/member/MemberApplicationTests.java
@@ -1,31 +1,13 @@
 package com.example.member;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultMatcher;
-
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.jsonPath;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
-@AutoConfigureMockMvc
 class MemberApplicationTests {
 
     @Test
     void contextLoads() {
     }
 
-    @Autowired
-    MockMvc mockMvc;
-
-    @Test
-    void healthEndpointReturnsUp() throws Exception {
-        mockMvc.perform(get("/actuator/health"))
-                .andExpect(status().isOk())
-                .andExpect((ResultMatcher) jsonPath("$.status").value("UP"));
-    }
 }
